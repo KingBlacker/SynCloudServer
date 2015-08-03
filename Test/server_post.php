060a2252456454c1dd7c5879e9d2345e72dff9b1
zhpmatrix <zhpmatrix@gmail.com>
2015-08-03 13:48:29 +0800

    Added by zhpmatrix:##############

--- Test/server_post.php
+++ Test/server_post.php
@@ -16,10 +16,13 @@
    	$password = $_POST['password'];
   
 	$query = "insert into ".$tableName." values('".$username."',".$password.",'".date('Y-m-d H:i:s',time())."');";
-    $result = mysql_query($query,$conn);
-    if(!$result){
-                  die("mysql error:".mysql_error());
-    }
-  
-    echo "add information to database sucessfullly!";
+  $result = mysql_query($query,$conn);
+  if(!$result){
+      die("mysql error:".mysql_error());
+  }
+  $string = "add information successfully...";
+  $resTips["tips"] = $string;
+  //$dt["username"] = $_GET['username'];
+  echo json_encode($resTips);
+  //echo "add information to database sucessfullly!";
 ?>
